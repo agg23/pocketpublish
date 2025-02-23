d68268e60775d5a89c844874420b2d88afcb4b58
Adam Gastineau <adam@agg.im>
2025-02-23 07:01:48 -0800

    Add argument to disable release creation

--- release.py
+++ release.py
@@ -1,15 +1,20 @@
 from helpers import *
+import argparse
 
 
 def main():
     print("Releasing")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--norelease", action="store_true", required=False)
+    args = parser.parse_args()
     # Load gateware.json file
     config = read_gateware_json()
     # Create zip files for distribution
     pkg_file = create_release_package(config, "pocket")
     meta_file = create_metadata_package(config, "pocket")
     # Create GitHub release
-    release_urls = create_gh_release(config, [pkg_file, meta_file])
+    if not args.norelease:
+        release_urls = create_gh_release(config, [pkg_file, meta_file])
     # Send Discord announcement
     # send_discord_announcement(config, release_urls)
 
