55dff61f77a9d16ba3e4f42679b3c6426e0fc718
Mike Frysinger <vapier@gmail.com>
2026-09-08 19:06:06 +0545

    add proper option parsing to all tools (#3)
    This makes standard things like --help just work which makes it easier
    to understand what each one actually does -- without it making changes.

--- release.py
+++ release.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 
-from helpers import *
+"""Generate release artifacts.
+
+This assumes it's running in a GitHub action with various GITHUB_* env vars.
+"""
+
 import argparse
+import sys
+
+from helpers import *
 
 
-def main():
-    print("Releasing")
-    parser = argparse.ArgumentParser()
+def main(argv: list[str]) -> int | None:
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--norelease", action="store_true", required=False)
-    args = parser.parse_args()
+    opts = parser.parse_args(argv)
+
+    print("Releasing")
     # Load gateware.json file
     config = read_gateware_json()
     # Create zip files for distribution
     pkg_file = create_release_package(config, "pocket")
     meta_file = create_metadata_package(config, "pocket")
     # Create GitHub release
-    if not args.norelease:
+    if not opts.norelease:
         release_urls = create_gh_release(config, [pkg_file, meta_file])
     # Send Discord announcement
     # send_discord_announcement(config, release_urls)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv[1:]))
--- reverse.py
+++ reverse.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
+"""Reverses the bitstream of an RBF file."""
+
+import argparse
+import sys
+
 from helpers import *
-from sys import argv
 
 
-def main():
+def main(argv: list[str]) -> int | None:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("input")
+    parser.add_argument("output")
+    opts = parser.parse_args(argv)
+
     print("Reversing")
-    reverse_bitstream(argv[1], argv[2])
+    reverse_bitstream(opts.input, opts.output)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv[1:]))
